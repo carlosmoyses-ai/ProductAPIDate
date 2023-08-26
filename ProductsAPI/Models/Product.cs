8f94532391f9e31e326d7f8f0546caabdc0299ac
Carlos Moyses <carlosmoyses@gmail.com>
2023-08-26 20:10:34 -0300

    Adicionando validação no Product.cs

--- ProductsAPI/Models/Product.cs
+++ ProductsAPI/Models/Product.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ProductsAPI.Models;
 
 public class Product
 {
     public int ProductId { get; set; }
 
+    [StringLength(100)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
     public string? Name { get; set; }
 
+    [Range(1, 1000)]
+    [Required(ErrorMessage = "O preço é obrigatório")]   
     public decimal Price { get; set; }
 
+    [Required(ErrorMessage = "A quantidade é obrigatória")]
+    [Range(1, 1000)]
     public int Quantity { get; set; }
 }
